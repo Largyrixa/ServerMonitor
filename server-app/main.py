53038fb5499f81cfd3421c3754dbd1638ab276a4
Largyrixa <lopes.m.iago@gmail.com>
2025-12-23 16:11:24 -0300

    Criação do protótipo de server-app

---
+++ server-app/main.py
@@ -0,0 +1,34 @@
+from os import system
+from time import sleep
+
+import serial
+
+
+def desligar_server():
+    # system("sudo shuttdown now")
+    print("desligando...")
+
+
+# Configuração da serialport: str = "/dev/ttyACM0"
+ser = serial.Serial(port, baudrate=9600, timeout=1)
+
+# Loop principal
+while True:
+    try:
+        if ser.in_waiting:
+            line: str = ser.readline().decode().strip()
+            response = ""
+
+            if not line:
+                continue
+            elif line.upper().strip() == "DESLIGAR":
+                desligar_server()
+                response = "DESLIGANDO\n"
+            else:
+                response = "ERRO\n"
+            ser.write(response.encode())
+            print(line)
+            print(response)
+    except Exception as e:
+        print(e)
+    sleep(1)
