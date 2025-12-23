d798567ad5bd2b09bc90da5e6c8174e4a7c23889
Largyrixa <lopes.m.iago@gmail.com>
2025-12-23 16:11:03 -0300

    REFACTOR: mudança do tipo do dado na ServerState

--- src/definitions.h
+++ src/definitions.h
@@ -5,6 +5,6 @@
   #define RELAY_PIN 4
 #endif
 
-enum class ServerState : char { LIGANDO, LIGADO, DESLIGANDO, DESLIGADO };
+enum class ServerState : uint8_t { LIGANDO, LIGADO, DESLIGANDO, DESLIGADO };
 
 #endif
