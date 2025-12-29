ed5b40eb0027b5a819bf7dbc26c560f9ece3d1ce
Largyrixa <lopes.m.iago@gmail.com>
2025-12-29 20:45:22 -0300

    definitions: Muda o enum ServerState

--- src/definitions.h
+++ src/definitions.h
@@ -1,10 +1,23 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+#include <Arduino.h>
+
 #ifndef RELAY_PIN
   #define RELAY_PIN 4
 #endif
 
-enum class ServerState : uint8_t { LIGANDO, LIGADO, DESLIGANDO, DESLIGADO, ERRO };
+#ifndef SERIAL_BAUD
+  #define SERIAL_BAUD 9600UL
+#endif
+
+enum class ServerState : uint8_t {
+    BOOTING = 0,
+    ACTIVE,
+    SHUTTING_DOWN,
+    INACTIVE,
+    ERROR,
+    _COUNT
+};
 
 #endif
