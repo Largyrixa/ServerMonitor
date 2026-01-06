507cc411bf9591ceb496add66cd75a96e05bf132
Largyrixa <lopes.m.iago@gmail.com>
2026-01-06 17:56:15 -0300

    Organização dos arquivos

--- src/serverState.h
+++ src/serverState.h
@@ -1,15 +1,7 @@
-#ifndef DEFINITIONS_H
-#define DEFINITIONS_H
+#ifndef SERVERSTATE_H
+#define SERVERSTATE_H
 
 #include <Arduino.h>
-
-#ifndef RELAY_PIN
-  #define RELAY_PIN 4
-#endif
-
-#ifndef SERIAL_BAUD
-  #define SERIAL_BAUD 9600UL
-#endif
 
 enum class ServerState : uint8_t {
     BOOTING = 0,
