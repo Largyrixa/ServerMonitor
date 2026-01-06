0c3776b4d816be12445bc9d1f0802998f2d087de
Largyrixa <lopes.m.iago@gmail.com>
2026-01-06 17:57:20 -0300

    Atualização do arquivo principal, provavelmente a versão final

--- ServerMonitor.ino
+++ ServerMonitor.ino
@@ -1,14 +1,33 @@
-#include "src/definitions.h"
+#include "src/serverState.h"
+#include "src/environment.h"
 #include "src/relayController.h"
 #include "src/serverController.h"
 
 #include <WiFiManager.h>
+#include <ESP32Servo.h>
 
-RelayController relay(RELAY_PIN, true);
-ServerController *server;
+using namespace std;
 
-void setup() 
-{
+// Funções de controle do servidor
+bool powerServerOn();
+bool powerServerOff();
+ServerState pingServer();
+
+// Cliente WiFi e bot do telegram
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+// Controlador do servidor
+ServerController server(bot, powerServerOn, powerServerOff, pingServer);
+
+// Servo motor que aciona o botão
+#define SERVO_PIN 4
+Servo servo;
+
+
+#define SERIAL_BAUD 115200
+
+void setup() {
   Serial.begin(SERIAL_BAUD);
   Serial.println();
   Serial.flush();
@@ -20,22 +39,77 @@ void setup()
     ESP.restart();
   }
 
-  // pinMode(2, INPUT);
-
-  // server = new ServerController(&relay);
-
-  relay.pulse(100);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  server.begin();
+  servo.attach(SERVO_PIN);
 }
 
-void loop() 
-{
+void loop() {
   const String s = Serial.readStringUntil('\n');
 
   if (s == "ligar") {
-    relay.turnOn();
+    server.powerOn();
   } else if (s == "desligar") {
-    relay.turnOff();
-  } else if (s == "pulso") {
-    relay.pulse(500);
+    server.powerOff();
+  } else if (s == "ping") {
+    auto state = server.getState();
+    switch (state) {
+      case ServerState::ACTIVE:
+        Serial.println("Servidor Ativo");
+        break;
+      case ServerState::INACTIVE:
+        Serial.println("Servidor Inativo");
+        break;
+      default:
+        Serial.println("Servidor com Erro");
+        break;
+    }
   }
+}
+
+// Definição das funções de controle do servidor
+bool powerServerOn() {
+  // Aperta o botão devagar
+  const int MAX_ANG = 20;
+  for (int ang = 0; ang <= MAX_ANG; ang++) {
+    servo.write(ang);
+    delay(10);
+  }
+  delay(100);
+
+  servo.write(0);
+  delay(100);
+
+  return true;
+}
+
+bool powerServerOff() {
+  // Manda o comando de desligar
+  Serial.println("/desligar");
+  Serial.flush();
+  delay(100);
+
+  // Lê a resposta do servidor
+  // para verificar o recebimento da mensagem
+  const auto response = Serial.readStringUntil('\n');
+  if (response == "OK")
+    return true;
+  else
+    return false;
+}
+
+ServerState pingServer() {
+  // Manda o comando de ping
+  Serial.println("/ping");
+  Serial.flush();
+  delay(100);
+
+  // Lê a resposta do servidor
+  const String response = Serial.readStringUntil('\n');
+  if (response == "OK")
+    return ServerState::ACTIVE;
+  else if (response == "ERRO")
+    return ServerState::ERROR;
+  else  // Se não recebemos mensagem, servidor desligado
+    return ServerState::INACTIVE;
 }
